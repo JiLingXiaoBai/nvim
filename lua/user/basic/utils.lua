f4d352c6815e50f308a4f21a3f876493211c4b4c
JiLingXiaoBai <UIOJKL3588@QQ.com>
2022-10-07 17:53:11 +0800

    fix keymap bugs

--- lua/user/basic/utils.lua
+++ lua/user/basic/utils.lua
@@ -48,6 +48,38 @@ M.close_buffer = function()
     end
 end
 
+M.close_buffer_num = function(num)
+    local buflisted = vim.fn.buflisted
+    local bufnr = vim.fn.bufnr
+    local nvim_list_bufs = vim.api.nvim_list_bufs()
+    local execute = vim.cmd
+
+    local oldnr = bufnr('%')
+    execute ("BufferLineGoToBuffer"..num)
+    local curnr = bufnr('%')
+
+    local bufnum = 0
+    for i, v in pairs(nvim_list_bufs) do
+        if buflisted(v) == 1 then
+            bufnum = bufnum + 1
+        end
+    end
+    if num > bufnum then
+        return
+    end
+    if oldnr == curnr then
+        if bufnum == 1 then
+            execute "quit"
+            return
+        else
+            execute "bp|bd#"
+            return
+        end
+    else
+        execute ("buffer"..oldnr.."|bd#")
+    end
+end
+
 M.last_position = function()
     local line = vim.fn.line
     if line("'\"") > 1 and line("'\"") <= line("$") then
