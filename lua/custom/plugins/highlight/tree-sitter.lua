bfc16f70312450f36afb4eb39db2cebe35e0de6f
Xiao Bai <uiojkl3588@qq.com>
2025-06-04 18:59:52 +0800

    tree-sitter switch to main branch

--- lua/custom/plugins/highlight/tree-sitter.lua
+++ lua/custom/plugins/highlight/tree-sitter.lua
@@ -1,26 +1,28 @@
 -- Highlight, edit, and navigate code
 return {
   'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-  -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-  opts = {
-    ensure_installed = { 'bash', 'c', 'cpp', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-    -- Autoinstall languages that are not installed
-    auto_install = true,
-    highlight = {
-      enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { 'ruby' },
-    },
-    indent = { enable = true, disable = { 'ruby' } },
-  },
-  -- There are additional nvim-treesitter modules that you can use to interact
-  -- with nvim-treesitter. You should go explore a few and see what interests you:
-  --
-  --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-  --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-  --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+  branch = 'main',
+  config = function()
+    local nvim_treesitter = require 'nvim-treesitter'
+    nvim_treesitter.setup()
+
+    local ensure_installed = { 'bash', 'c', 'cpp', 'c_sharp', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
+    local pattern = {}
+    for _, parser in ipairs(ensure_installed) do
+      local has_parser, _ = pcall(vim.treesitter.language.inspect, parser)
+
+      if not has_parser then
+        nvim_treesitter.install(parser)
+      else
+        pattern = vim.tbl_extend('keep', pattern, vim.treesitter.language.get_filetypes(parser))
+      end
+    end
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = pattern,
+      callback = function()
+        vim.treesitter.start()
+      end,
+    })
+    vim.api.nvim_exec_autocmds('FileType', {})
+  end,
 }
